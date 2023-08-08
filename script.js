fe49a524669f6c6e542cba463982f6e702391ab6
Arbaj-09 <141715228+Arbaj-09@users.noreply.github.com>
2023-08-08 17:47:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const cells = document.querySelectorAll('.cell');
+  let currentPlayer = 'X';
+  
+  cells.forEach(cell => {
+    cell.addEventListener('click', handleCellClick);
+  });
+  
+  function handleCellClick(e) {
+    const cell = e.target;
+    if (cell.textContent !== '') return;
+    
+    cell.textContent = currentPlayer;
+    cell.classList.add(currentPlayer);
+    
+    if (checkWin()) {
+      alert(`${currentPlayer} wins!`);
+      resetGame();
+      return;
+    }
+    
+    if (checkDraw()) {
+      alert("It's a draw!");
+      resetGame();
+      return;
+    }
+    
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    
+    if (currentPlayer === 'O') {
+      // Call the function for computer's move here
+      // For simplicity, let's assume computer selects random available cell
+      const availableCells = Array.from(cells).filter(cell => cell.textContent === '');
+      const randomCell = availableCells[Math.floor(Math.random() * availableCells.length)];
+      randomCell.click();
+    }
+  }
+  
+  function checkWin() {
+    const winningCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+      [0, 4, 8], [2, 4, 6] // diagonals
+    ];
+    
+    return winningCombinations.some(combination => {
+      const [a, b, c] = combination;
+      return cells[a].textContent === currentPlayer && 
+             cells[b].textContent === currentPlayer && 
+             cells[c].textContent === currentPlayer;
+    });
+  }
+  
+  function checkDraw() {
+    return Array.from(cells).every(cell => cell.textContent !== '');
+  }
+  
+  function resetGame() {
+    cells.forEach(cell => {
+      cell.textContent = '';
+      cell.classList.remove('X', 'O');
+    });
+    currentPlayer = 'X';
+  }
+});
